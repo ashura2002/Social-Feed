2f8ae571b1164d2b607a9f77b69d15eca149a0e8
Jmark Dayna <jemarcdayna352@gmail.com>
2025-11-25 12:57:05 +0800

    added posts modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './modules/users/users.controller';
 import { UsersModule } from './modules/users/users.module';
+import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PostsModule],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
