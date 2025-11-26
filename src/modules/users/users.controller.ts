be0cac4b4ba7b82b81fc97bcf62d0c7b424d3064
Jmark Dayna <jemarcdayna352@gmail.com>
2025-11-26 19:16:02 +0800

    test added methods on user service

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,11 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDTO } from './dto/request/create-user.dto';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { User } from './entity/user.entity';
+
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Post()
-  async createUser(@Body() createDTO: CreateUserDTO) {}
+  async createUser(@Body() createDTO: CreateUserDTO):Promise<User> {
+    return await this.userService.create(createDTO);
+  }
 }
